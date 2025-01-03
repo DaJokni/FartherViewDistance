dba62d0dc1c671729e64ac138dcfa223ca0e358a
jokni <56400974+DaJokni@users.noreply.github.com>
2025-01-03 15:34:42 +0200

    update paperweight

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     java
     `maven-publish`
-    id("io.papermc.paperweight.userdev") version "1.7.7"
-    id("com.gradleup.shadow") version "8.3.5"
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.11"
+    id("com.gradleup.shadow") version "9.0.0-beta4"
 }
 
 repositories {
